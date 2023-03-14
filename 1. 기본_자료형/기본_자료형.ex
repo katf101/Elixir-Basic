474a61b133c7e80f7a2fd52058fe9eee5a99a96e
hansol <vawav11@gmail.com>
2023-03-14 23:32:18 +0900

    Update 기본_자료형.ex

--- 1. 기본_자료형/기본_자료형.ex
+++ 1. 기본_자료형/기본_자료형.ex
@@ -1,7 +1,7 @@
 # 기본 자료형과 변수 선언 방법
-nuber = 123
-float = 3.14
-boolean = true
-atom = :ok
-atom = :error
-string = "Hello, Elixir!"
+nuber = 123 # 정수형
+float = 3.14 # 부동 소수점형
+boolean = true # 불리언
+atom = :ok # 원자(Atom)
+atom = :error # 원자(Atom)
+string = "Hello, Elixir!" # 문자열
